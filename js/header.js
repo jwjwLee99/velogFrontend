b72096305ab189221771010deb6a4c5474ff112b
DESKTOP-EVDUNQ5\LJW <ljwwnd99@gmail.com>
2022-09-20 11:32:58 +0900

    header.js 작업완료, index의 추가 스크롤메뉴 작업 필요

--- js/header.js
+++ js/header.js
@@ -1,1 +1,21 @@
 // header.js
+$(() => {
+    let previousScroll = 0;
+    let sh = $("#scrollMoveHeader");
+    
+    $(window).scroll(function() {
+        let currentScroll = $(this).scrollTop();
+    
+        if(currentScroll < previousScroll){
+            sh.css("display", "block")
+            sh.fadeIn(500)
+            console.log("up")
+        }else {
+            sh.fadeIn(1000)
+            sh.css("display", "none")
+            console.log("down")
+        }
+        previousScroll = currentScroll;
+    })
+})
+
