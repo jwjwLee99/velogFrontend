ff6bd889c8e5d99d1919903ecbdf263b0e3afd5f
yhw0104 <yhw0104@naver.com>
2022-09-27 16:59:27 +0900

    트렌딩 최신 변경

--- js/index.js
+++ js/index.js
@@ -1,1 +1,30 @@
 // index.js
+$(() => {
+    let trandingBtn = $(".tranding")
+    let latestBtn = $(".latest")
+
+    let main1 = $(".main-list")
+    let main2 = $(".main-list2")
+
+    let underline = $(".underLine")
+
+    trandingBtn.click(()=> {
+        trandingBtn.addClass("active")
+        latestBtn.removeClass("active")
+        main1.css("display", "flex")
+        main2.css("display", "none")
+        underline.animate({
+            left: "0%"
+        }, 300)
+    })
+
+    latestBtn.click(()=> {
+        trandingBtn.removeClass("active")
+        latestBtn.addClass("active")
+        main1.css("display", "none")
+        main2.css("display", "flex")
+        underline.animate({
+            left: "50%"
+        }, 300)
+    })
+})
