b617dd968fde87a62f6dc726990ae80b9b5030bd
junhyuk920 <junhyuk920@naver.com>
2022-10-13 19:12:06 +0900

    준혁 push

--- js/post/postwrite.js
+++ js/post/postwrite.js
@@ -1,6 +1,46 @@
+$(() => {
+    var input = document.querySelector('.basic'),
+        tagify = new Tagify(input, {
+        })
+    tagify.addEmptyTag()
+}
+)
 
 $(() => {
-    $("#tag-input").focus(function(){
-        $(".tag-modal").css("display", "block");
+    let main1 = $(".wrapper")
+    let main2 = $(".cPEGOG")
+    let main3 = $(".dTTjYG")
+    let publicationBtn = $(".jYsOEX")
+    let cancleBtn = $(".bitsEI")
+
+    publicationBtn.click(() => {
+        cancleBtn.removeClass("active")
+        publicationBtn.addClass("active")
+        main3.animate({
+            top: "-50%"
+        }, 300)
+        main1.css("display", "none")
+        main2.css("display", "flex")
     })
+
+    cancleBtn.click(() => {
+        cancleBtn.removeClass("active")
+        publicationBtn.addClass("active")
+        main1.css("display", "block")
+        main2.css("display", "none")
+    })
+}
+)
+
+
+$(() => {
+    document.getElementById("b2").onclick = function () {
+        document.getElementById("b1").className = "bXGcfH";
+        document.getElementById("b2").className = "yAuOg";
+    }
+
+    document.getElementById("b1").onclick = function () {
+        document.getElementById("b1").className = "yAuOg";
+        document.getElementById("b2").className = "bXGcfH";
+    }
 })
